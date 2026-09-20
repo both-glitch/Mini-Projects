8aa4ee5cdd26f0c4097c5557b521986eea401309
Both <moyvirakboth@gmail.com>
2026-09-20 21:06:40 +0700

    Is there password input

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ checkButton.addEventListener("click", function(){
     const getPassword = document.getElementById("password-input").value;
     const strengthText = document.getElementById("strength-text");
     const bar = document.querySelector(".strength-fill");
+    const valid = true;
     const checks = {
         lengths: getPassword.length >= 8,
         lowercase: /[a-z]/.test(getPassword),
@@ -19,16 +20,27 @@ checkButton.addEventListener("click", function(){
         strengthText.textContent = "Medium";
         bar.style.width = "50%";
     }
-    else{
+    else if (passed == 1){
         strengthText.textContent = "Weak";
-        bar.style.width = "0%";
+        bar.style.width = "25%";
     }
+    else if (passed == 0){
+        strengthText.textContent = "Please enter password";
+        strengthText.style.color = "Red";
+        setTimeout(() => {
+            strengthText.style.color = "#64748b";
+            strengthText.textContent = "-";
+        }, 2000)
+        valid = false;
+    }
+
     const length = document.getElementById("eight-char");
     const upper = document.getElementById("upper-char");
     const lower = document.getElementById("lower-char");
     const number = document.getElementById("number-char");
 
-    if (checks.lengths){
+    if(valid){
+        if (checks.lengths){
         length.style.color = "#1cbd57";
     }else{
         length.style.color = "#bd1c1c";
@@ -47,6 +59,7 @@ checkButton.addEventListener("click", function(){
         number.style.color = "#1cbd57";
     }else{
         number.style.color = "#bd1c1c";
+    }
     }
 }
 );
